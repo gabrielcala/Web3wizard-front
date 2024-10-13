05a791c820bd91417051d6f3a655b576aa52b366
Dione-b <dionenassau321@gmail.com>
2024-10-13 17:31:01 -0300

    teste

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import React from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiProvider } from 'wagmi';
-import  config from './config.js';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+} from "@thirdweb-dev/react";
 
-const queryClient = new QueryClient();
-
+// Crie uma instância do QueryClient
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        {/** Aqui você pode adicionar seus componentes */}
+    <QueryClientProvider client={queryClient}>
+      <ThirdwebProvider
+        supportedWallets={[
+          metamaskWallet({
+            recommended: true,
+          }),
+          coinbaseWallet(),
+          walletConnect(),
+        ]}
+        clientId={import.meta.env.VITE_REACT_CLIENT_ID}
+      >
         <App />
-      </QueryClientProvider>
-    </WagmiProvider>
-    
+      </ThirdwebProvider>
+    </QueryClientProvider>
   </StrictMode>,
-
 )
