6bbd16d2b000a311e94d42cdb571b241ebbd403f
Dione-b <dionenassau321@gmail.com>
2024-10-14 11:17:47 -0300

    style: add rainbow kit and chackra ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
-import React, { useState } from 'react';
-import './App.css'; // Importando o CSS
-import { getMetamaskProvider } from './metamaskService';
+import React, { useState } from "react";
+import "./App.css"; // Importando o CSS
+import { getMetamaskProvider } from "./metamaskService";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { Box, Button, Input, InputGroup, Stack } from "@chakra-ui/react";
 
 const Dojo = () => {
-    const [texto, setTexto] = useState('');
-    const [output, setOutput] = useState('');
+  const [texto, setTexto] = useState("");
+  const [output, setOutput] = useState("");
 
-    const mostrarTexto = () => {
-        setOutput(texto);
-    };
+  const mostrarTexto = () => {
+    setOutput(texto);
+  };
 
-    return (
-        <div className="papel">
-            <div className="output" id="outputText">{output}</div>
-            <input
-                type="text"
-                id="campo"
-                name="campo"
-                style={{ height: '30px' }}
-                value={texto}
-                onChange={(e) => setTexto(e.target.value)}
-            />
-            <br /><br />
-            <button onClick={mostrarTexto} type="button">Update</button>
-            <button onClick={() => getMetamaskProvider()} type="button">Conectar</button>
-        </div>
-
-    );
-
-    
+  return (
+    <div className="papel">
+      <Box
+        bg="white"
+        w="50%"
+        h="20%"
+        p={4}
+        marginTop={10}
+        id="outputText"
+        color="black"
+        rounded={10}
+      >
+        {output}
+      </Box>
+      <Stack spacing={2}>
+        <InputGroup>
+          <Input
+            placeholder="Send text"
+            onChange={(e) => setTexto(e.target.value)}
+            value={texto}
+            bg="white"
+          />
+        </InputGroup>
+        <Button size="lg" onClick={mostrarTexto}>
+          Update
+        </Button>
+      </Stack>
+      <Stack>
+        <ConnectButton />
+      </Stack>
+    </div>
+  );
 };
 
 export default Dojo;
