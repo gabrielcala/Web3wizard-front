9f8aeb387bf678a2cfbd8fb6eedcc5bc7232b452
Gabriel Gustavo Calá <calagabriel6@gmail.com>
2024-10-17 11:53:48 -0300

    intaracao

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 import React, { useState } from "react";
+import { ethers, Signer } from 'ethers';
 import "./App.css"; // Importando o CSS
 import { getMetamaskProvider } from "./metamaskService";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Box, Button, Input, InputGroup, Stack } from "@chakra-ui/react";
+import ABI from './abi.json';
+import { providers } from "web3";
 
 const Dojo = () => {
   const [texto, setTexto] = useState("");
   const [output, setOutput] = useState("");
+  const [message, setMessage] = useState("");
 
-  const mostrarTexto = () => {
-    setOutput(texto);
+  const CONTRACT_ADDRESS = "0x0e736a5c39B7Cdf798cdd2A3fa7d88Cf4f07804e"
+
+  async function mostrarTexto() {
+      
+    
+    try {
+      const provider = await getDefaultProvider();
+      const signer = provider.getSigner();
+
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
+      const contractSigner = contract.connect(signer);
+
+      const tx = await contractSigner.sendMessage({String});
+      setOutput(JSON.stringify(tx));
+
+      console.log(signer);
+
+
+    }
+    catch (err) {
+      setMessage(err.message);
+    }
   };
 
   return (
--- src/metamaskService.js
+++ src/metamaskService.js
@@ -4,8 +4,10 @@ export async function getMetamaskProvider() {
     if(!window.ethereum) throw new Error(`Metamask not detected`);
 
     const web3 = new Web3(window.ethereum);
+    
 
     const accounts = await web3.eth.requestAccounts();
+    console.log(accounts);
     if(!accounts || !accounts.length) throw new Error(`Permission required`);
 
     return web3;
